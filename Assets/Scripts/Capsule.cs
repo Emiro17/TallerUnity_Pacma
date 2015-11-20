d3c0c7f692895aecced4c46236ef28a19e363473
emiro17 <darkast117@hotmail.com>
2015-11-20 11:46:11 -0500

    primer entrada workshop Pac-Vivelab

---
+++ Assets/Scripts/Capsule.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Capsule : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		gameObject.SetActive(false);
+		Debug.Log ("Chocapic");
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
